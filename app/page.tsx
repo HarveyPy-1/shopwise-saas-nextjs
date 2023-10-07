ead9c0c573916a2cc903fd81824897aab42b2e1f
Harvey Ezihe <don4dolex@gmail.com>
2023-10-07 19:25:27 +0100

    Add Carousel section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import HeroCarousel from "@/components/HeroCarousel";
 import SearchBar from "@/components/SearchBar"
 import Image from "next/image";
 
 const Home = () => {
 	return (
 		<>
-			<section className="px-6 md:px-20 py-24 border-2">
+			<section className="px-6 md:px-20 py-24"> 
 				<div className="flex max-xl:flex-col gap-16">
 					<div className="flex flex-col justify-center">
 						<p className="small-text">
@@ -26,7 +27,7 @@ const Home = () => {
 						</p>
 						<SearchBar />
 					</div>
-					HeroCarousel
+					<HeroCarousel />
 				</div>
 			</section>
 
