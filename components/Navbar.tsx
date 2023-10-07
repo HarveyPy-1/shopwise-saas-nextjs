a800a2d4bd42251eed610c94bdfc70d6ee3819c7
Harvey Ezihe <don4dolex@gmail.com>
2023-10-07 17:15:06 +0100

    Add NavBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// Get the navbar icons
+const navIcons = [
+	{ src: "/assets/icons/search.svg", alt: "search" },
+	{ src: "/assets/icons/black-heart.svg", alt: "heart" },
+	{ src: "/assets/icons/user.svg", alt: "user" },
+];
+
 const Navbar = () => {
 	return (
 		<header className="w-full">
-			<nav>Navbar</nav>
+			<nav className="nav">
+				<Link href="/" className="flex items-center gap-1">
+					<Image
+						src="/assets/icons/logo.svg"
+						width={27}
+						height={27}
+						alt="logo"
+					/>
+					<p className="nav-logo">
+						Shop<span className="text-primary">Wise</span> PriceTracker
+					</p>
+				</Link>
+				<div className="flex items-center gap-5">
+					{navIcons.map((icon) => (
+						<Image
+							key={icon.alt}
+							src={icon.src}
+							alt="icon.alt"
+							width={28}
+							height={28}
+							className="object-contain"
+						/>
+					))}
+				</div>
+			</nav>
 		</header>
 	);
 };
