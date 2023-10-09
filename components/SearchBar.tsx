9708389c6d3fdd42614775cb366e015c870e8261
Harvey Ezihe <don4dolex@gmail.com>
2023-10-09 21:30:14 +0100

    Add validation logic to search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,62 @@
-"use client"; // We have to add this because we added a function(interactivity) to our form in the client facing side. NextJS likes things to be separated and functionality to be on the server side.
+"use client";
+// We have to add this because we added a function(interactivity) to our form in the client facing side. NextJS likes things to be separated and functionality to be on the server side.
+import { FormEvent, useState } from "react";
+
+// True/False
+const isValidAmazonProductURL = (url: string) => {
+	try {
+		const parsedURL = new URL(url);
+		const hostname = parsedURL.hostname;
+
+		if (
+			hostname.includes("amazon.com") ||
+			hostname.includes("amazon.") ||
+			hostname.endsWith("amazon")
+		) {
+			return true;
+		}
+	} catch (error) {
+		return false;
+	}
+};
 
 const SearchBar = () => {
-	const handleSubmit = () => {};
+	const [searchPrompt, setSearchPrompt] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
+	// We need to define what type of data event is, hence the FormEvent...
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		const isLinkValid = isValidAmazonProductURL(searchPrompt);
+
+		if (!isLinkValid) return alert("Please provide a valid Amazon Link");
+
+		try {
+			setIsLoading(true);
+
+			// Scrape the website
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
 			<input
 				type="text"
+				value={searchPrompt}
+				onChange={(e) => setSearchPrompt(e.target.value)}
 				placeholder="Enter product link"
 				className="searchbar-input"
 			/>
-			<button type="submit" className="searchbar-btn">
-				Search
+			<button
+				type="submit"
+				className="searchbar-btn"
+				disabled={searchPrompt === ""}>
+				{isLoading ? "Searching..." : "Search"}
 			</button>
 		</form>
 	);
